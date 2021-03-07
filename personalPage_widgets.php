f890b5ac8d761fd683e1f8032c5dc416ced4faaa
Juan Kaenel <kaenel10@gmail.com>
2021-03-07 14:39:17 -0300

    :art: Añadiendo formulario de cantidad de cursos al widget

--- personalPage_widgets.php
+++ personalPage_widgets.php
@@ -36,29 +36,77 @@ class personalPage_cursos_Widget extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
-		echo $args['before_widget'];
+        echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
-		echo esc_html__( 'Hello, World!', 'text_domain' );
+        $cantidad = $instance['cantidad']; // cantidad de cursos ingresados en el formulario
+        if($cantidad == ''){
+            $cantidad = 3;
+        }
+        
+        ?>
+
+        <ul>
+            <?php 
+                $args = array(
+                        'post_type' => 'personalPage_Cursos',
+                        'posts_per_page'=> $cantidad,
+                        /* 'posts_per_page' => 3, */
+                       /*  'orderby' => 'rand' */
+                    );
+                    $cursos = new WP_Query($args);
+                    while($cursos->have_posts()): $cursos->the_post();
+                ?>
+                <li class="curso-sidebar">
+                    <div class="imagen">
+                        <?php the_post_thumbnail( 'thumbnail');?>
+                    </div>
+
+                    <div class="contenido-curso">
+                        <a href="<?php the_permalink(); ?>">
+                            <h3><?php the_title(); ?></h3>
+                        </a>
+                        <p><?php the_field('descripcion_del_curso'); ?></p>
+                    </div>
+                </li>
+
+                <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+
+
+        <?php 
 		echo $args['after_widget'];
 	}
+
 
 	/**
 	 * Back-end widget form.
 	 *
 	 * @see WP_Widget::form()
 	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
-		?>
-		<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
-		<?php 
+	 * @param array $instance Previously saved values from database. 
+     * 	 
+     * */
+
+    /**
+    * Desplegaremos un formulario para agregar cantidad de cursos en el menú de widgets-sidebars
+    **/
+	public function form( $instance ) { 
+		$cantidad = !empty($instance['cantidad']) ? $instance['cantidad'] : esc_html__('Cuántos cursos deseas mostrar?', 'personalPage'); ?>
+        
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id('cantidad') ) ?>"></label>
+                <?php esc_attr_e('¿Cuántos Cursos deseas mostrar?', 'personalPage') ?>
+        
+            <input 
+            class="widefat" 
+            id="<?php echo esc_attr( $this->get_field_id( 'cantidad' ) ); ?>" 
+            name="<?php echo esc_attr( $this->get_field_name( 'cantidad' ) ); ?>" 
+            type="number" 
+            value="<?php echo esc_attr( $cantidad ); ?>" >
+        </p>
+    <?php 
 	}
 
 	/**
@@ -71,9 +119,10 @@ class personalPage_cursos_Widget extends WP_Widget {
 	 *
 	 * @return array Updated safe values to be saved.
 	 */
+    /*Almacenará los datos del formulario */
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+		$instance['cantidad'] = ( ! empty( $new_instance['cantidad'] ) ) ? sanitize_text_field( $new_instance['cantidad'] ) : '';
 
 		return $instance;
 	}
